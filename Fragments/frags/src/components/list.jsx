919e0036dcffd78637c3a871edca7c15aeb04ce7
Arslaan Ahmad <139920302+Arslaan027@users.noreply.github.com>
2024-07-13 16:38:55 +0530

    routine push

--- Fragments/frags/src/components/list.jsx
+++ Fragments/frags/src/components/list.jsx
@@ -1,13 +1,19 @@
 function Listitem() {
-  let foodItems = ["Dal", "Roti", "Bhindi", "Torai", "Sabzi"];
+  let foodItems = ["Dal", "Roti", "Bhindi", "Torai", "Sabzi", "gosht"];
+
   return (
-    <ul className="list-group list">
-      {foodItems.map((item) => (
-        <li key={item} className="list-group-item">
-          {item}
-        </li>
-      ))}
-    </ul>
+    <>
+      {foodItems.length === 0 ? (
+        <h3>please Add something in the Array</h3>
+      ) : null}
+      <ul className="list-group list">
+        {foodItems.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 export default Listitem;
