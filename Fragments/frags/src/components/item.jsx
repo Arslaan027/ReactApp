3de9aa9b0d0e5d66f94d530c459f8c24cecaa292
Arslaan Ahmad <139920302+Arslaan027@users.noreply.github.com>
2024-07-16 12:00:09 +0530

    btn function added

--- Fragments/frags/src/components/item.jsx
+++ Fragments/frags/src/components/item.jsx
@@ -1,17 +1,13 @@
 import css from "./item.module.css";
 
-function Item({ Items }) {
-  const handleBuyButton = (e) => {
-    console.log(e);
-    console.log(`${Items}` + " bought");
-  };
+function Item({ Items, handleBuyButton }) {
   return (
     <>
       <li className={`${css["kg-class"]} list-group-item`}>
         <span className={css["kg-span"]}>{Items}</span>
         <button
           className={` ${css.buttons} btn btn-success`}
-          onClick={(e) => handleBuyButton(e)}
+          onClick={handleBuyButton}
         >
           Buy
         </button>
--- Fragments/frags/src/components/listItem.jsx
+++ Fragments/frags/src/components/listItem.jsx
@@ -1,10 +1,14 @@
 import Item from "./item";
 
-function Listitem({ Items }) {
+function Listitem({ Items, handleBuyButton }) {
   return (
     <ul className="list-group list">
       {Items.map((item) => (
-        <Item key={item} Items={item} />
+        <Item
+          key={item}
+          Items={item}
+          handleBuyButton={(event) => console.log("you bought " + `${item}`)}
+        />
       ))}
     </ul>
   );
