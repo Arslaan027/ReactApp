582e9e742fa44075812a3d7c3493e3b04efafbcb
Arslaan Ahmad <139920302+Arslaan027@users.noreply.github.com>
2024-07-19 10:10:29 +0530

    new updates

--- Fragments/frags/src/components/Inputfood.jsx
+++ Fragments/frags/src/components/Inputfood.jsx
@@ -1,15 +1,11 @@
 import style from "./Inputfood.module.css";
 
-<<<<<<< HEAD
-function Input({ onEnter, newEnter }) {
-=======
 function Input({ value, onChange, onEnter }) {
-  const handleKeyDown= (event) => {
-    if(event.key === "Enter"){
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
       onEnter(event);
     }
   };
->>>>>>> df17eab149e325459c4b81f181d0c1054d28f550
   return (
     <>
       <center>
@@ -17,14 +13,9 @@ function Input({ value, onChange, onEnter }) {
           className={style.searchBox}
           placeholder="Enter your food"
           type="text"
-<<<<<<< HEAD
-          newEnter={newEnter}
-          onKeyDown={onEnter}
-=======
           value={value}
           onChange={onChange}
           onKeyDown={handleKeyDown}
->>>>>>> df17eab149e325459c4b81f181d0c1054d28f550
         />
       </center>
     </>
