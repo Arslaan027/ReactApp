828bc1915471832d950e8fd2e595ca1723229065
Arslaan Ahmad <139920302+Arslaan027@users.noreply.github.com>
2024-07-19 12:19:06 +0530

    useState

--- reactHooks/useState/src/App.jsx
+++ reactHooks/useState/src/App.jsx
@@ -1,30 +1,34 @@
 import { useState } from "react";
 
 function App() {
-  let [color, setColor] = useState("Red");
+  let [data, setData] = useState({
+    framework: "React",
+    languege: "JavaScript",
+  });
 
-  function colorButton() {
-    console.log("btn");
-    if (color === "Red") {
-      setColor("Blue");
-    } else {
-      setColor("Red");
-    }
-  }
+  const handleButton = () => {
+    setData((data) => {
+      return {
+        ...data,
+        languege: "C#",
+        framework: ".Net",
+      };
+    });
+  };
 
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <div className="shadow-2xl ring-slate-700 max-w-2xl rounded-3xl p-10 bg-white">
           <h2 className="flex justify-center p-10 mx-20">
-            my Favorite color is {color}
+            this is {data.framework} framwork of {data.languege}
           </h2>
           <div className="flex justify-center">
             <button
-              onClick={colorButton}
+              onClick={handleButton}
               className="rounded-2xl px-5 py-1 bg-blue-300 hover:bg-blue-500 focus:ring focus:ring-green-400"
             >
-              {color}
+              Next {data.framework}
             </button>
           </div>
         </div>
