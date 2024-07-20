c1aa2ba3488a82ab4aa603032a2940959bdc26c5
Arslaan Ahmad <arslaanahmadrajput@gmail.com>
2024-07-20 11:21:18 +0530

    cube function added

--- reactHooks/useMemo/src/App.jsx
+++ reactHooks/useMemo/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
 
-  let [numberCube, setNumber] = useState(0);
+  let [number, setNumber] = useState(0);
   let [count, setCount] = useState(0);
 
   function numberCube (num) {
@@ -11,12 +11,14 @@ function App() {
     return Math.pow(num, 3);
   }
 
-  let result = numberCube(numberCube);
+  let result = numberCube(number);
 
   return (
     <>
 
-
+    <input className='m-5 border border-black border-solid font-bold px-8 py-2 text-[25px]' value={number} type='number' onChange={(e)=>{setNumber(e.target.value)}}/>
+    <br />
+    <h1 className='text-lg my-5 '>the cube of the number is <span className='bg-green-300 p-3 rounded-lg font-bold'>{result}</span> </h1>
       <button className='text-2xl rounded-2xl focus:ring focus:to-blue-200 px-4 py-2 bg-green-300 hover:bg-green-400'>Click me</button>
     </>
   )
