ead51e819f29bd49699986a628c40ab7cbc968c6
Arslaan Ahmad <139920302+Arslaan027@users.noreply.github.com>
2024-07-20 16:44:17 +0530

    New System

--- reactHooks/useMemo/src/App.jsx
+++ reactHooks/useMemo/src/App.jsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // let [numberCube, setNumber] = useState(0);
+  // let [count, setCount] = useState(0);
 
-  let [numberCube, setNumber] = useState(0);
-  let [count, setCount] = useState(0);
-
-  function numberCube (num) {
-    console.log("calculation done!");
-    return Math.pow(num, 3);
-  }
-
-  let result = numberCube(numberCube);
+  // function numberCube (num) {
+  //   console.log("calculation done!");
+  //   return Math.pow(num, 3);
+  // }
 
   return (
     <>
-
-
-      <button className='text-2xl rounded-2xl focus:ring focus:to-blue-200 px-4 py-2 bg-green-300 hover:bg-green-400'>Click me</button>
+      <button className="text-2xl rounded-2xl focus:ring focus:to-blue-900 px-4 py-2 bg-green-300 hover:bg-green-400">
+        Click me
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
