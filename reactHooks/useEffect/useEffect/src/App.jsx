a425395fd272c2b20d242731bcf2749a5633aa13
Arslaan Ahmad <139920302+Arslaan027@users.noreply.github.com>
2024-07-20 10:46:12 +0530

    switching device

--- reactHooks/useEffect/useEffect/src/App.jsx
+++ reactHooks/useEffect/useEffect/src/App.jsx
@@ -5,18 +5,22 @@ function App() {
   let [currentEmployeeIndex, setCurrentEmployeeIndex] = useState(0);
   let [employee, setEmployee] = useState([
     {
+      empID: "1001",
       name: "salman",
       age: "26",
     },
     {
+      empID: "1002",
       name: "arslaan",
       age: "24",
     },
     {
+      empID: "1003",
       name: "nabeel",
       age: "28",
     },
     {
+      empID: "1004",
       name: "kashif",
       age: "28",
     },
@@ -27,7 +31,7 @@ function App() {
     setTimeout(() => {
       setCount((prev) => prev + 1);
       setCurrentEmployeeIndex((prevIndex) => (prevIndex + 1) % employee.length);
-    }, 1000);
+    }, 2000);
   };
 
   useEffect(Timer, [count]);
@@ -37,8 +41,10 @@ function App() {
       <div className="flex justify-center">
         <p className="text-center mt-[50vh] text-2xl px-5 py-4 bg-orange-500 w-[80%]  rounded-3xl">
           Counter{" "}
-          <span className="bg-blue-500 px-4 py-2 rounded-xl">{count}</span> Name
-          of the Employee is{" "}
+          <span className="bg-blue-500 px-4 py-2 rounded-xl">
+            {currentEmployeeIndex + 1}
+          </span>{" "}
+          Name of the Employee is{" "}
           <span className="bg-blue-500 px-4 py-2 rounded-xl">
             {employee[currentEmployeeIndex].name}
           </span>{" "}
